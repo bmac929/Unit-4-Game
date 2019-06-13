286b7a8af337731f3b28ea1d10d7d47ba54fcbf4
Bryan McCann <bmac929@yahoo.com>
2019-06-13 09:59:43 -0500

    Final Edits. Corrected front end and got the on click function to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     var randomNumber = Math.floor(Math.random() * (101) + 19);
     var wins = 0;
     var losses = 0;
+    var totalScore = 0;
     $("#randNumber").html(randomNumber);
     $("#winNumber").html(wins);
     $("#lossNumber").html(losses);
@@ -15,38 +16,55 @@ $(document).ready(function () {
 
     $("#blueGem").on("click", function () {
         totalScore = totalScore + blue;
+        console.log(totalScore);
+        updateScore();
+        checkScore();
     })
 
     $("#purpleGem").on("click", function () {
         totalScore = totalScore + purple;
+        updateScore();
+        checkScore();
     })
     $("#greenGem").on("click", function () {
         totalScore = totalScore + green;
+        updateScore();
+        checkScore();
     })
     $("#whiteGem").on("click", function () {
         totalScore = totalScore + white;
+        updateScore();
+        checkScore();
     })
 
-    if (totalScore === randomNumber) {
-        wins++;
-        $("#winNumber").html(wins);
-        alert("You Win!");
-    }
-    if (totalScore > randomNumber) {
-        losses++;
-        $("#lossNumber").html(losses);
-        alert("You Lose!");
-        reset;
+    function checkScore() {
+        if (totalScore === randomNumber) {
+            wins++;
+            $("#winNumber").html(wins);
+            alert("You Win!");
+            reset();
+        } else if (totalScore > randomNumber) {
+            losses++;
+            $("#lossNumber").html(losses);
+            alert("You Lose!");
+            reset();
+        }
     }
 
     function reset() {
         totalScore = 0;
-        var blue = Math.floor(Math.random() * (12));
-        var green = Math.floor(Math.random() * (12));
-        var white = Math.floor(Math.random() * (12));
-        var purple = Math.floor(Math.random() * (12));
+        blue = Math.floor(Math.random() * (12) + 1);
+        green = Math.floor(Math.random() * (12) + 1);
+        white = Math.floor(Math.random() * (12) + 1);
+        purple = Math.floor(Math.random() * (12) + 1);
 
-        var randomNumber = Math.floor(Math.random() * (101) + 19);
+        randomNumber = Math.floor(Math.random() * (102) + 19);
+        updateScore();
+        $("#randNumber").html(randomNumber);
+    }
+
+    function updateScore() {
+        $("#totalScore").html(totalScore);
     }
 
     /* function add() {
